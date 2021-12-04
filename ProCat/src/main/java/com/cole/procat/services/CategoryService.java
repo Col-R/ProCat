9b4273bb380835e9886ed36f52f6db8b4de113d4
Col-R <2colewestin@gmail.com>
2021-12-03 19:16:09 -0600

    created add and get queries for CategoryService

--- ProCat/src/main/java/com/cole/procat/services/CategoryService.java
+++ ProCat/src/main/java/com/cole/procat/services/CategoryService.java
@@ -1,5 +1,28 @@
 package com.cole.procat.services;
 
-public class CategoryService {
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.cole.procat.models.Category;
+import com.cole.procat.repositories.CategoryRepository;
+
+@Service
+public class CategoryService {
+	@Autowired
+	private CategoryRepository cRepo;
+	
+	public List<Category> getAllCategories(){
+		return this.cRepo.findAll();
+	}
+	// get one category
+	public Category getOneCategory(Long id) {
+		return this.cRepo.findById(id).orElse(null);
+	}
+	// create Category
+	public Category createCategory(Category category) {
+		return this.cRepo.save(category);
+	}
+	
 }
