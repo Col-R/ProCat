c2893b0fe0dbea3fff212ddc4d24e725adf43691
Col-R <2colewestin@gmail.com>
2021-12-03 19:20:10 -0600

    created add and get queries for ProductService

--- ProCat/src/main/java/com/cole/procat/services/ProductService.java
+++ ProCat/src/main/java/com/cole/procat/services/ProductService.java
@@ -1,5 +1,27 @@
 package com.cole.procat.services;
 
-public class ProductService {
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.cole.procat.models.Product;
+import com.cole.procat.repositories.ProductRepository;
+
+@Service
+public class ProductService {
+	@Autowired
+	private ProductRepository pRepo;
+	
+	public List<Product> getAllProducts(){
+		return this.pRepo.findAll();
+	}
+	// get one product
+	public Product getOneProduct(Long id) {
+		return this.pRepo.findById(id).orElse(null);
+	}
+	// create product
+	public Product createProduct(Product product) {
+		return this.pRepo.save(product);
+	}
 }
