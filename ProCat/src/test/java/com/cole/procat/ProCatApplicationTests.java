55c44a6f5d05bff88af071462f88ed01d3b79f68
Col-R <2colewestin@gmail.com>
2021-12-03 18:26:26 -0600

    initial commit

---
+++ ProCat/src/test/java/com/cole/procat/ProCatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cole.procat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProCatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
