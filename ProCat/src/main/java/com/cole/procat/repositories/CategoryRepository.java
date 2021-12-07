8ef2594a9dd8d99f20316e72d49e7465028e69ec
Col-R <2colewestin@gmail.com>
2021-12-06 20:21:36 -0600

    added not contains repo

--- ProCat/src/main/java/com/cole/procat/repositories/CategoryRepository.java
+++ ProCat/src/main/java/com/cole/procat/repositories/CategoryRepository.java
@@ -6,8 +6,10 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import com.cole.procat.models.Category;
+import com.cole.procat.models.Product;
 
 @Repository
 public interface CategoryRepository extends CrudRepository<Category, Long>{
 	List<Category> findAll();
+	List<Category> findByProductsNotContains(Product product);
 }
